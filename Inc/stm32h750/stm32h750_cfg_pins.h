6c8d944d4265af4ef0ddc9394be3bb7241f49261
Stonerose35 <fuerholz@gmx.ch>
2024-03-30 00:47:34 +0100

    changed clipping led pins

--- Inc/stm32h750/stm32h750_cfg_pins.h
+++ Inc/stm32h750/stm32h750_cfg_pins.h
@@ -49,9 +49,11 @@
 #define I2C_ENR RCC_APB1LENR_I2C1EN_Pos
 #endif
 
-#define POWERSENSE_PIN (0*0 + 0)
+#define PCM3060_DEEMPHASIS (1*16 + 11) // PB11
+
+#define POWERSENSE_PIN (2*16 + 9) //PC9
 #define CLIPPING_LED_INPUT (2*16 + 10) //PC10
-#define CLIPPING_LED_OUTPUT (2*16 + 9) //PC11
+#define CLIPPING_LED_OUTPUT (2*16 + 11) //PC11
 
 //ssd1306 display interfaced using spi
 #define SSD1306_SCK (6*16+11) // PG11
